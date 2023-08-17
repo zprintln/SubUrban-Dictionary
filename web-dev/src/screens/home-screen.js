1caa5011a7b15cd16cd06a54911c51f658b85c8d
Karim Semaan <91396847+karimsemaan@users.noreply.github.com>
2023-08-17 13:03:59 +0300

    changes to home,login,search,

--- web-dev/src/screens/home-screen.js
+++ web-dev/src/screens/home-screen.js
@@ -1,7 +1,35 @@
-const HomeScreen = () => {
-    return (<div>
-        Home
-    </div>)
-};
+import React, { useState } from 'react';
+import { AiOutlineSearch } from 'react-icons/ai';
+import { performSearch } from '../services/search-service';
 
-export default HomeScreen;
+const HomeScreen = () => {
+    const [searchTerm, setSearchTerm] = useState('');
+    const [results, setResults] = useState([]);
+  
+    return (
+      <div className="row">
+        <div className="col-11 position-relative">
+          <input
+            placeholder="Search a definition"
+            className="form-control rounded-pill ps-5"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+          <AiOutlineSearch
+            className="fs-3 position-absolute"
+            style={{ left: '25px', bottom: '3px' }}
+            onClick={() => performSearch(searchTerm, setResults)} // Call with parameters
+          />
+        </div>
+        {/* Render the search results */}
+        <div>
+          {results.map((result, index) => (
+            <div key={index}>{result.definition}</div> 
+          ))}
+        </div>
+        {/* Other content of the home screen */}
+      </div>
+    );
+  };
+  
+  export default HomeScreen;
