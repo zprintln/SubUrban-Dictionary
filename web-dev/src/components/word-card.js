492c6b3d09b234c617a6250a6f7f32da3fd94eff
Karim Semaan <91396847+karimsemaan@users.noreply.github.com>
2023-08-18 18:42:42 +0300

    remove the thunks and used the services directly.

--- web-dev/src/components/word-card.js
+++ web-dev/src/components/word-card.js
@@ -1,14 +1,36 @@
-import React from "react";
-import { BiBookmark } from "react-icons/bi";
-import { Button } from "react-bootstrap";
-import { useDispatch } from "react-redux";
-import { deleteWordDefinitionThunk, addFavoriteWordThunk } from "../services/word-reducer";
+import React, { useState } from 'react';
+import { BiBookmark } from 'react-icons/bi';
+import { Button } from 'react-bootstrap';
+import { deleteWordDefinition, addFavoriteWord } from '../services/word-service'; 
 
 const WordCard = ({ wordDetails, showDeleteButton, showSaveButton, isSaved }) => {
-  const dispatch = useDispatch();
-
+  const [isLoading, setIsLoading] = useState(false);
   const postedDate = new Date(wordDetails.posted_at);
   const formattedDate = postedDate.toDateString();
+
+  const handleSave = async () => {
+    setIsLoading(true);
+    try {
+      await addFavoriteWord(wordDetails);
+      alert('Word saved to your favorites!'); 
+    } catch (error) {
+      alert('Something went wrong. Please try again later.');
+      console.error(error); 
+    }
+    setIsLoading(false);
+  };
+
+  const handleDelete = async () => {
+    setIsLoading(true);
+    try {
+      await deleteWordDefinition(wordDetails.id);
+      alert('Word definition unpublished successfully.'); 
+    } catch (error) {
+      alert('Something went wrong. Please try again later.'); 
+      console.error(error); 
+    }
+    setIsLoading(false);
+  };
 
   return (
     <div className="card">
@@ -18,16 +40,17 @@ const WordCard = ({ wordDetails, showDeleteButton, showSaveButton, isSaved }) =>
         <p><em>{wordDetails.example}</em></p>
         <p><b>by&nbsp;<span className="text-primary">{wordDetails.user}</span>{formattedDate}</b></p>
         {showSaveButton && (
-          <Button variant="btn btn-outline-success" onClick={() => dispatch(addFavoriteWordThunk(wordDetails))}>
+          <Button variant="btn btn-outline-success" onClick={handleSave} disabled={isLoading}>
             <BiBookmark /> {isSaved ? "Saved" : "Save"}
           </Button>
         )}
         &nbsp;
         {showDeleteButton && (
-          <Button variant="btn btn-outline-danger" className="ml-2" onClick={() => dispatch(deleteWordDefinitionThunk(wordDetails.id))}>
+          <Button variant="btn btn-outline-danger" className="ml-2" onClick={handleDelete} disabled={isLoading}>
             Unpublish
           </Button>
         )}
+        {isLoading && <span>Loading...</span>}
       </div>
     </div>
   );
