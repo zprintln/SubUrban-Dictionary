278a160f547823ffd75e0234ace9f2ac8dec08d6
Shai Mann-Robison <mann.robison@gmail.com>
2023-08-21 23:22:07 -0400

    Update profile-screen.js

--- web-dev/src/screens/profile-screen.js
+++ web-dev/src/screens/profile-screen.js
@@ -2,24 +2,13 @@ import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import WordCard from "../components/word-card";
 import * as wordService from "../services/word-service";
-import * as userService from "../services/user-service";
 import { useParams } from "react-router-dom";
 
 const ProfileScreen = () => {
   const { currentUser } = useSelector((state) => {
     return state.user;
   });
-
   const [favorites, setFavorites] = useState([]);
-  const [editMode, setEditMode] = useState(false);
-  const [updatedUsername, setUpdatedUsername] = useState(
-    currentUser?.username || ""
-  );
-  const [updatedPassword, setUpdatedPassword] = useState("");
-  const [updatedIsModerator, setUpdatedIsModerator] = useState(
-    currentUser?.isModerator || false
-  );
-
   const { username } = useParams();
 
   useEffect(() => {
@@ -44,19 +33,6 @@ const ProfileScreen = () => {
     setFavorites(favorites.filter((w) => w._id !== id));
   };
 
-  const handleUpdateInfo = async () => {
-    try {
-      await userService.updateUserInfo(currentUser.username, {
-        username: updatedUsername,
-        password: updatedPassword,
-        isModerator: updatedIsModerator,
-      });
-      setEditMode(false);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
   return (
     <div>
       {currentUser && (
@@ -69,30 +45,6 @@ const ProfileScreen = () => {
       <h4 style={{ textTransform: "capitalize" }} className="text-primary">
         {currentUser ? "My" : username + "'s "} Favorites
       </h4>
-      {editMode && (
-        <div>
-          <input
-            type="text"
-            value={updatedUsername}
-            onChange={(e) => setUpdatedUsername(e.target.value)}
-            placeholder="Username"
-          />
-          <input
-            type="password"
-            value={updatedPassword}
-            onChange={(e) => setUpdatedPassword(e.target.value)}
-            placeholder="Password"
-          />
-          <input
-            type="checkbox"
-            checked={updatedIsModerator}
-            onChange={(e) => setUpdatedIsModerator(e.target.checked)}
-          />
-          <label>Moderator</label>
-          <button onClick={handleUpdateInfo}>Save</button>
-          <button onClick={() => setEditMode(false)}>Cancel</button>
-        </div>
-      )}
       {/* Conditional rendering if favorites exist */}
       {favorites.length > 0 ? (
         // Map through the favorite words and render WordCard components
